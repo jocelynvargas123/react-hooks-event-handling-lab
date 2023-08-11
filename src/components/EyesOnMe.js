8fc009e3b0fcb6e2e0b2ea81769d1406bc9a9a38
Jocelyn <jocelynvargas123@yahoo.com>
2023-08-10 19:12:12 -0700

    "test passed"

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,16 @@
-// Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe(){
+    const [fieldValue, setFieldValue] = React.useState('')
+    const handleBlur = (e) => {
+        setFieldValue(e.target.value)
+        return (
+            console.log("Hey! Eyes on me!")
+        )
+    }
+    return (
+        <button onFocus={()=> console.log("Good!")} onBlur={handleBlur}>Eyes on me</button>
+    )
+}
+
+export default EyesOnMe
